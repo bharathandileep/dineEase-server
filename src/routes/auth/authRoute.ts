64408ed307d7183b2fac4591ddd1e6472cfb4411
bharathandileep <bharathan.dileepkumar@inciem.com>
2025-01-24 13:29:46 +0530

    google auth and otp generate

--- src/routes/auth/authRoute.ts
+++ src/routes/auth/authRoute.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express from "express";
 import { apiConfig } from "../../config/endpoint ";
 import { verifyFirebaseToken } from "../../middleware/verifyFirebaseToken";
 import {
