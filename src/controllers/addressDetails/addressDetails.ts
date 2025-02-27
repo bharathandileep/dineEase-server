51d394e37fd16804b7da0092b8f54bb4d9cf8092
vismaya-vijayan25 <vismayasvijayan252003@gmail.com>
2025-02-27 16:40:50 +0530

    address details setted

--- src/controllers/addressDetails/addressDetails.ts
+++ src/controllers/addressDetails/addressDetails.ts
@@ -14,106 +14,116 @@ import State from "../../models/state/StateModel";
 import City from "../../models/city/City";
 import District from "../../models/district/District";
 
-export const getAllCountries = async (req: Request, res: Response): Promise<void> => {
-    try {
-      console.log("inside getallcountries controller");
-      
-      const countries = await Country.find();
-      console.log(countries)
-      
-      res.status(200).json({
-        success: true,
-        data: countries,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        success: false,
-        message: "An error occurred while fetching the countries.",
-      });
-    }
-  };
-  
-  export const getStatesByCountry = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { countryName } = req.params;
-      const states = await State.find({ country_name: countryName });
-  
-      if (states.length === 0) {
-        res.status(404).json({
-          success: false,
-          message: `No states found for country: ${countryName}`,
-        });
-        return;
-      }
-  
-      res.status(200).json({
-        success: true,
-        data: states,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        success: false,
-        message: "An error occurred while fetching the states.",
-      });
-    }
-  };
-  
-  export const getCitiesByState = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { stateName } = req.params;
-      console.log(stateName,"heloo");
-      
-      const cities = await City.find({ state_id: stateName });
-  console.log(cities);
-  
-      if (cities.length === 0) {
-        res.status(404).json({
-          success: false,
-          message: `No cities found for state ID: ${stateName}`,
-        });
-        return;
-      }
-  
-      res.status(200).json({
-        success: true,
-        data: cities,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        success: false,
-        message: "An error occurred while fetching the cities.",
-      });
-    }
-  };
-  
-export const getDistrictsByState = async(req:Request,res:Response):Promise<void>=>{
-    try{
-        const{stateId} = req.params;
-        const districts = await District.find({state_id:stateId});
+export const getAllCountries = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    console.log("inside getallcountries controller");
 
-        if(districts.length==0){
-            res.status(404).json({
-                success:false,
-                message:`No districts found for state Id:${stateId}`,
-            });
-            return
-        }
-        res.status(200).json({
-            success:true,
-            data:districts,
-        });
+    const countries = await Country.find();
+    console.log(countries);
+
+    res.status(200).json({
+      success: true,
+      data: countries,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching the countries.",
+    });
+  }
+};
+
+export const getStatesByCountry = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { countryName } = req.params;
+    const states = await State.find({ country_id: Number(countryName) });
+    console.log(states);
+    if (states.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: `No states found for country: ${countryName}`,
+      });
+      return;
     }
-    catch(error){
-        console.error(error);
-        res.status(500).json({
-            success:false,
-            message:"An error occured while fetching the districts"
-        });
-        
+
+    res.status(200).json({
+      success: true,
+      data: states,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching the states.",
+    });
+  }
+};
+
+export const getCitiesByState = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { stateName } = req.params;
+    console.log(stateName, "heloo");
+
+    const cities = await City.find({ state_id: Number(stateName) });
+    console.log(cities);
+
+    if (cities.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: `No cities found for state ID: ${stateName}`,
+      });
+      return;
     }
+
+    res.status(200).json({
+      success: true,
+      data: cities,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while fetching the cities.",
+    });
+  }
+};
+
+export const getDistrictsByState = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { stateId } = req.params;
+    const districts = await District.find({ state_id: stateId });
+
+    if (districts.length == 0) {
+      res.status(404).json({
+        success: false,
+        message: `No districts found for state Id:${stateId}`,
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      data: districts,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occured while fetching the districts",
+    });
+  }
 };
 
 // export const getcitiesByDistricts = async(req:Request,res:Response):Promise<void>=>{
--- src/models/state/StateModel.ts
+++ src/models/state/StateModel.ts
@@ -6,18 +6,20 @@ export interface IState extends Document {
   //state_id: number; // Another unique identifier (if needed)
   name: string;
   country_name: string; // Name of the country the state belongs to
+  country_id:number
 }
 
 // Create the schema for the State model
-const stateSchema: Schema<IState> = new Schema<IState>(
-  {
-    id: { type: Number, unique: true }, // Unique state ID
-    //state_id: { type: Number, unique: true }, // Another identifier (optional)
-    name: { type: String, required: true, unique: true }, // State name
-    country_name: { type: String, required: true } // Country name reference
-  },
-  { timestamps: true }
-);
+  const stateSchema: Schema<IState> = new Schema<IState>(
+    {
+      id: { type: Number, unique: true }, // Unique state ID
+      //state_id: { type: Number, unique: true }, // Another identifier (optional)
+      name: { type: String, required: true, unique: true }, // State name
+      country_id: { type: Number, required: true, unique: true }, // State name
+      country_name: { type: String, required: true } // Country name reference
+    },
+    { timestamps: true }
+  );
 
 // Create the model for the State schema
 const State: Model<IState> = mongoose.model<IState>("State", stateSchema);
